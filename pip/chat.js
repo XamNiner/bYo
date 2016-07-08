e6d80d98c5204064b7f480ad21db6672b9a8553b
XamNiner <xamniner@fivedigits.net>
2016-07-08 10:52:51 +0200

    peer connection events

--- pip/chat.js
+++ pip/chat.js
@@ -2,7 +2,7 @@ var app = angular.module('chatApp', [])
 
 
 app.factory('socket', function() {
-    var socket = io.connect('http://p2psocket.heroku.com');
+    var socket = io.connect();
     return socket;
 })
 
